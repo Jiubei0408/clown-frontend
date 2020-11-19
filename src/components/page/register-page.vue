fd40899a070200280e2438496e2fbe4e68e3894e
Jiubei0408 <757795210@qq.com>
2020-11-19 14:37:50 +0800

    添加注册界面

--- src/components/page/register-page.vue
+++ src/components/page/register-page.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>注册</div>
+  <div
+      class="fill-background"
+      style="width: 100%; height: 100%;"
+      :style="{'background-image': `url(${background_image}`}">
+  </div>
 </template>
 
 <script>
 export default {
-  name: "register-page"
+  name: "register-page",
+  data() {
+    return {
+      background_image: require("@/assets/register-background.png")
+    }
+  }
 }
 </script>
 
